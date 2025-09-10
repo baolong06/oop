af3563a53d5622dd514c8dead8059c570beab55d
baolong06 <baolongid2006@gmail.com>
2025-09-10 17:21:32 +0700

    update: thêm/sửa bài làm <mô tả ngắn>

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,10 +1,31 @@
-﻿internal class Program
+﻿using System;
+namespace lai_suat
 {
-    
-    private static void Main(string[] args)
+    class program
     {
-        MyDate md1 = new MyDate(DateTime.Now.Day,DateTime.Now.Month, DateTime.Now.Year);
-        Console.WriteLine("{0}/{1}/{2}",md1.Day,md1.Month,md1.Year);
-        Console.ReadKey();
+        static void Main(string[] args)
+        {
+            string masv, hoten;
+            float diem1, diem2, diem3;
+            do
+            {
+                Console.Write("nhap ma sinh vien: ");
+                masv = Console.ReadLine();
+                Console.Write("nhap ho ten sinh vien: ");
+                hoten = Console.ReadLine();
+            } while (masv == "" || hoten == "");
+            do
+            {
+                Console.Write("nhap diem mon 1: ");
+                diem1 = float.Parse(Console.ReadLine());
+                Console.Write("nhap diem mon 2: ");
+                diem2 = float.Parse(Console.ReadLine());
+                Console.Write("nhap diem mon 3: ");
+                diem3 = float.Parse(Console.ReadLine());
+            } while ((diem1 < 0 || diem1 > 10) || (diem2 < 0 || diem2 > 10) || (diem3 < 0 || diem3 > 10));
+            Console.WriteLine("ma sinh vien la: {0}, ho ten sinh vien la {1}", masv, hoten);
+            Console.WriteLine("diem mon 1 la: {0}, diem mon 2 la: {1}, diem mon 3 la: {2}", diem1, diem2, diem3);
+            Console.ReadKey();
+        }
     }
 }
