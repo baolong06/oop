f4f49b98133e298eacf9ae2f7e59a7d460630e21
baolong06 <baolongid2006@gmail.com>
2025-09-11 22:19:33 +0700

    Update Program.cs

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,31 +1,9 @@
-﻿using System;
-namespace lai_suat
+using System;
+
+class Program
 {
-    class program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            string masv, hoten;
-            float diem1, diem2, diem3;
-            do
-            {
-                Console.Write("nhap ma sinh vien: ");
-                masv = Console.ReadLine();
-                Console.Write("nhap ho ten sinh vien: ");
-                hoten = Console.ReadLine();
-            } while (masv == "" || hoten == "");
-            do
-            {
-                Console.Write("nhap diem mon 1: ");
-                diem1 = float.Parse(Console.ReadLine());
-                Console.Write("nhap diem mon 2: ");
-                diem2 = float.Parse(Console.ReadLine());
-                Console.Write("nhap diem mon 3: ");
-                diem3 = float.Parse(Console.ReadLine());
-            } while ((diem1 < 0 || diem1 > 10) || (diem2 < 0 || diem2 > 10) || (diem3 < 0 || diem3 > 10));
-            Console.WriteLine("ma sinh vien la: {0}, ho ten sinh vien la {1}", masv, hoten);
-            Console.WriteLine("diem mon 1 la: {0}, diem mon 2 la: {1}, diem mon 3 la: {2}", diem1, diem2, diem3);
-            Console.ReadKey();
-        }
+        Console.WriteLine("Lương Bảo Long\t12424022\t12424TN\tbaolong06\tbaolongid2006@gmail.com");
     }
 }
