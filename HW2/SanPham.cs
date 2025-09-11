f7ef566654f92f5dba1c0f691a11df5bb3d6c5b5
baolong06 <baolongid2006@gmail.com>
2025-09-11 10:31:25 +0700

    Add files via upload

---
+++ HW2/SanPham.cs
@@ -0,0 +1,24 @@
+﻿
+internal class SanPham
+{
+    private string masp;
+    private string tensp;
+    private double giaban;
+    private int soluongton;
+    public SanPham(string masp, string tensp, double giaban, int slton)
+    {
+        this.masp = masp;
+        this.tensp = tensp;
+        this.giaban = giaban;
+        this.soluongton = slton;
+    }
+    public void InSanPham()
+    {
+        Console.WriteLine("MaSP={0}, TenSP ={1}, Gia ban={2}, So luong con={3}", masp, tensp, giaban, soluongton);
+    }
+    public bool KiemTraDatHang(int thr)
+    {
+        return soluongton <thr;
+    }
+}
+
